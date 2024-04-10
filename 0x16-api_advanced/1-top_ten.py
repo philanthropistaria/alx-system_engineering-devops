e25c229810c5f1c9092da239eb6b199269c31bf8
philanthropistaria <aatandoh56@gmail.com>
2024-04-10 13:01:58 -0700

    hi

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Function to query subscribers on
+a given Reddit subreddit."""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    """Return the top ten hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+        return
+    results = response.json().get("data").get("children")
+    for post in results:
+        print(post.get("data").get("title"))
